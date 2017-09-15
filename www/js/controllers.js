e632382abd07735ac368d2de337fc6905c86cb12
kyungjoongo <kyungjoon.go@gmail.com>
2017-09-15 19:05:23 +0900

    35643563456

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,133 @@
 angular.module('starter.controllers', [])
+  .controller('DashCtrl', function ($scope, $http, DashService) {
 
-.controller('DashCtrl', function($scope) {})
+    DashService.getAll(1).then(function (response) { //2. so you can use .then()
+      $scope.items = response.data.resultList;
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+      /*$scope.items=response.products;*/
+    });
+    $scope.isMoreItems = false;
+    $scope.items2 = [];
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    $scope.loadMore = function () {
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+      DashService.getAll(1).then(function (response) { //2. so you can use .then()
+        $scope.item2 = response.data.resultList;
+      });
+
+
+      $scope.$apply(function () {
+        $scope.items = $scope.items.concat($scope.item2);
+      });
+
+      $scope.isMoreItems = true;
+
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+
+    }
+
+  })
+
+  .controller('ChatsCtrl', function ($scope, Chats) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+      Chats.remove(chat);
+    };
+  })
+
+  .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+  })
+
+
+  /**
+   *#####################
+   * 임대 컨트롤러....................
+   * ######################
+   */
+  .controller('AccountCtrl', function ($scope, DashService, $state, $location) {
+
+
+    $scope.itemsAvailable = true;
+    $scope.pageIndex = 1;
+    $scope.items = [];
+
+
+    $scope.loadMore = function () {
+
+      DashService.getAll($scope.pageIndex).then(function (response) { //2. so you can use .then()
+        $scope.items2 = response.data.resultList;
+
+        for (var i = 0; i < $scope.items2.length; i++) {
+          $scope.items.push($scope.items2[i]);
+        }
+        $scope.pageIndex++;
+
+        console.log("pageIndex--->" + $scope.pageIndex);
+
+        /*if ($scope.items.length == 99) {
+          //더이상의 아이템이 있는지 여부 (더이상아이템이 no가능==> true)
+          $scope.noMoreItemsAvailable = true;
+        }*/
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+      });
+    };
+
+    $scope.openInAppBrowser = function (parameters) {
+      var pblancId = parameters.pblancId;
+      // Open in app browser
+      window.open('https://www.myhome.go.kr/hws/portal/sch/selectRsdtRcritNtcDetailView.do?pblancId=' + pblancId, '_blank');
+    };
+
+    $scope.openCordovaWebView = function (parameters) {
+      var pblancId = parameters.pblancId;
+      // Open cordova webview if the url is in the whitelist otherwise opens in app browser
+      window.open('https://www.myhome.go.kr/hws/portal/sch/selectRsdtRcritNtcDetailView.do?pblancId=' + pblancId, '_selft');
+    };
+
+
+    $scope.goDetail = function (pblancId) {
+
+      console.log("pblancId---> " + pblancId);
+
+      $state.go("tab.imdeDetail", {pblancId: pblancId});
+
+
+    }
+
+  })
+
+  .controller('DetailCtrl', function ($scope, $stateParams, DetailService) {
+
+    console.log("pblancId333333333333333333-->" + $stateParams.pblancId);
+
+    $scope.pblancId = $stateParams.pblancId;
+
+    DetailService.getDetail($stateParams.pblancId).then(function (response) { //2. so you can use .then()
+      console.log(response.data.rcritBasic);
+
+      var rsult = response.data.rcritBasic;
+      $scope.guidanceCn = rsult.guidanceCn;
+
+      $scope.sttusNm = rsult.sttusNm;
+      $scope.suplyTyNm = rsult.suplyTyNm;
+      $scope.pblancNm = rsult.pblancNm;
+
+    });
+  })
+  .filter('newlines', function () {
+    return function(text) {
+      return text.replace(/\n/g, '<br/>');
+    }
+  });
+
+;
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,93 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+  .factory('Chats', function () {
+    // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
+    // Some fake testing data
+    var chats = [{
+      id: 0,
+      name: 'Ben Sparrow',
+      lastText: 'You on your way?',
+      face: 'img/ben.png'
+    }, {
+      id: 1,
+      name: 'Max Lynx',
+      lastText: 'Hey, it\'s me',
+      face: 'img/max.png'
+    }, {
+      id: 2,
+      name: 'Adam Bradleyson',
+      lastText: 'I should buy a boat',
+      face: 'img/adam.jpg'
+    }, {
+      id: 3,
+      name: 'Perry Governor',
+      lastText: 'Look at my mukluks!',
+      face: 'img/perry.png'
+    }, {
+      id: 4,
+      name: 'Mike Harrington',
+      lastText: 'This is wicked good ice cream.',
+      face: 'img/mike.png'
+    }, {
+      id: 5,
+      name: 'kyugjoon',
+      lastText: '고경준은 천재님이십니다sdlfkdlkfssldk is wicked good ice cream.',
+      face: 'img/mike.png'
     }
-  };
-});
+
+
+    ];
+
+    return {
+
+
+      all: function () {
+        return chats;
+      },
+      remove: function (chat) {
+        chats.splice(chats.indexOf(chat), 1);
+      },
+      get: function (chatId) {
+        for (var i = 0; i < chats.length; i++) {
+          if (chats[i].id === parseInt(chatId)) {
+            return chats[i];
+          }
+        }
+        return null;
+      }
+    };
+  })
+
+
+  .factory('DashService', function ($http) {
+    // Might use a resource here that returns a JSON array
+    return {
+      getAll: function (pageIndex) {
+        return $http.post("http://kyungjoon.ipdisk.co.kr:8080/lh/lhListToJson?pageIndex="+ pageIndex);  //1. this returns promise
+      }
+    };
+
+  })
+
+  .factory('DashService2', function ($http) {
+    // Might use a resource here that returns a JSON array
+    return {
+      getAll: function (pageIndex) {
+        return $http.post("http://kyungjoon.ipdisk.co.kr:8080/lh/lhListToJson?pageIndex=2");  //1. this returns promise
+      }
+    };
+
+  })
+
+  .factory('DetailService', function ($http) {
+    // Might use a resource here that returns a JSON array
+    return {
+      getDetail: function (pblancId) {
+        return $http.post("http://kyungjoon.ipdisk.co.kr:8080/lh/lhDetailToJson?pblancId="+ pblancId);  //1. this returns promise
+      }
+    };
+
+  })
+;
+
